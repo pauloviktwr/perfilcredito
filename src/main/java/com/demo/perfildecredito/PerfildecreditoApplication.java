32e481e6d898d740de0bdf34d7a6e5ffeec7bb43
victor <wvictor3011@gmail.com>
2025-04-24 22:07:32 -0300

    primeiro commit

---
+++ src/main/java/com/demo/perfildecredito/PerfildecreditoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.perfildecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfildecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfildecreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/perfildecredito/PerfildecreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.perfildecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfildecreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
